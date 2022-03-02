053c15a075a63b33475e5dc7c9d62e8c994c35ab
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-03-02 10:45:23 -0600

    update-amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "notereactaws": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "notereactawsd729077a": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3a774ccc8": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
