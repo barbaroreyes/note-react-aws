05922ccb0c643039f30a1b49248811f02bdfa3ba
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2022-03-02 10:48:32 -0600

    update-confi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-
+import Amplify from 'aws-amplify';
+import awsConfi from './aws-exports'
 import './App.css';
-
+Amplify.configure(awsConfi) ;
 function App() {
   return (
     <div className="App">
